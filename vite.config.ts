de264f2ee6cec909262418dae5b6b034250f4b8c
Your Name <you@example.com>
2026-02-16 00:10:53 +0530

    chore: update dependencies and configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "/",
-  
+
   server: {
     host: "::",
     port: 8080,
@@ -12,6 +13,7 @@ export default defineConfig({
 
   plugins: [
     react(),
+    tailwindcss(),
   ],
 
   resolve: {
